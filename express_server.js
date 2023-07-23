175824bad972d174239f9bb5452b4b45c3b58b78
Jubril Bello <jbello@uoguelph.ca>
2023-07-22 21:22:00 -0400

    Include cookie-parser to the sever

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
 
 const generateRandomString = function() {
   const characters =
