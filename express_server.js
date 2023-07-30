41d8b67b940a1f711c724e681c0e0a9dcc295574
Jubril Bello <jbello@uoguelph.ca>
2023-07-30 03:15:30 -0400

    Create user object

--- express_server.js
+++ express_server.js
@@ -3,7 +3,18 @@ const app = express();
 const PORT = 8080; // default port 8080
 const cookieParser = require('cookie-parser');
 
-
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
 
 const generateRandomString = function() {
   const characters =
